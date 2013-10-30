5df41b9cfa2bb1696317eeae1f607dc86c44c1fd
Yi Kong <yk1211@imperial.ac.uk>
2013-10-30 02:12:36 +0000

    Fix Unicode display issue under Qt4

--- symbol.hpp
+++ symbol.hpp
@@ -1,31 +1,31 @@
 #ifndef SYMBOL_HPP
 #define SYMBOL_HPP
 
-#define SYMBOL_NOT ("¬")
-#define SYMBOL_AND ("∧")
-#define SYMBOL_OR ("∨")
-#define SYMBOL_IMPLIES ("→")
-#define SYMBOL_IFF ("↔")
-#define SYMBOL_FALSITY ("⊥")
-#define SYMBOL_TRUTH ("⊤")
-#define SYMBOL_FORALL ("∀")
-#define SYMBOL_THEREEXISTS ("∃")
-#define SYMBOL_EQUALS ("=")
+#define SYMBOL_NOT (QChar(u'¬'))
+#define SYMBOL_AND (QChar(u'∧'))
+#define SYMBOL_OR (QChar(u'∨'))
+#define SYMBOL_IMPLIES (QChar(u'→'))
+#define SYMBOL_IFF (QChar(u'↔'))
+#define SYMBOL_FALSITY (QChar(u'⊥'))
+#define SYMBOL_TRUTH (QChar(u'⊤'))
+#define SYMBOL_FORALL (QChar(u'∀'))
+#define SYMBOL_THEREEXISTS (QChar(u'∃'))
+#define SYMBOL_EQUALS (QChar(u'='))
 
 enum Symbol {
-    NOT_SYMBOL,
-    AND_SYMBOL,
-    OR_SYMBOL,
-    IMPLIES_SYMBOL,
-    IFF_SYMBOL,
-    FALSITY_SYMBOL,
-    TRUTH_SYMBOL,
-    FORALL_SYMBOL,
-    THEREEXISTS_SYMBOL,
-    EQUALS_SYMBOL,
-    VARIABLE_SYMBOL,
-    PARAMETERS_SYMBOL,
-    PREDICATE_SYMBOL
+	NOT_SYMBOL,
+	AND_SYMBOL,
+	OR_SYMBOL,
+	IMPLIES_SYMBOL,
+	IFF_SYMBOL,
+	FALSITY_SYMBOL,
+	TRUTH_SYMBOL,
+	FORALL_SYMBOL,
+	THEREEXISTS_SYMBOL,
+	EQUALS_SYMBOL,
+	VARIABLE_SYMBOL,
+	PARAMETERS_SYMBOL,
+	PREDICATE_SYMBOL
 };
 
 #endif // SYMBOL_HPP
